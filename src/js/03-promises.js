77a8982a63dfa76dd008b7fea4b73f3b14ddd7b5
Germano1994 <125311105+Germano1994@users.noreply.github.com>
2023-07-07 18:31:30 +0300

    added files

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,55 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+document.querySelector(".form").addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector("input[name='delay']");
+  const stepInput = document.querySelector("input[name='step']");
+  const amountInput = document.querySelector("input[name='amount']");
+
+  const firstDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  const promises = generatePromises(amount, firstDelay, step);
+
+  Promise.all(promises)
+    .then((results) => {
+      console.log("All promises fulfilled:", results);
+    })
+    .catch((error) => {
+      console.log("At least one promise rejected:", error);
+    });
+}
+
+function generatePromises(amount, firstDelay, step) {
+  const promises = [];
+
+  for (let i = 0; i < amount; i++) {
+    const delay = firstDelay + i * step;
+    const promise = createPromise(i + 1, delay);
+    promises.push(promise);
+  }
+
+  return promises;
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      const result = { position, delay };
+
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, delay);
+  });
+}
